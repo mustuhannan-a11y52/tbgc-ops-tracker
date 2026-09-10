8f776ec9faa7b4d9ed79892cc635e375cb769fc9
mustuhannan-a11y52 <mustuhannan12345@gmail.com>
2026-09-10 12:35:24 +0530

    Create firebase-config.js

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase console → Project settings → General → Your apps → SDK setup and configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAvaChzwx1HU9TRSPSo8lsGqhRr5WeydPw",
+  authDomain: "tbgc---ops.firebaseapp.com",
+  projectId: "tbgc---ops",
+  storageBucket: "tbgc---ops.firebasestorage.app",
+  messagingSenderId: "554751326293",
+  appId: "1:554751326293:web:2e281a6eb4b67028967cdc",
+};
