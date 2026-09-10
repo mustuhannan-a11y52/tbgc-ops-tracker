75d694bbc34fec8e3c2819649b62ac7725ceee86
mustuhannan-a11y52 <mustuhannan12345@gmail.com>
2026-09-10 12:32:03 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import "./storage"; // sets window.storage before App mounts
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
